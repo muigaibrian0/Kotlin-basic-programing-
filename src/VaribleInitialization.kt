224da8d0a41e9456fa6dba5412ee1e8fc33c7966
muigaibrian0 <ljourney188@gmail.com>
2024-04-30 18:01:08 +0300

    intializing different variables

---
+++ src/VaribleInitialization.kt
@@ -0,0 +1,50 @@
+fun main(){
+    //declare string
+    val myString: String
+// declare Int
+    val myInt: Int
+// declare Long
+    val myLong: Long
+//declare byte
+    val myByte: Byte
+// declare short
+    val myShort: Short
+//declare Ubyte
+    val myUbyte: UByte
+//declare Ushort
+    val myUshort: UShort
+//declare uint
+    val myUint: UInt
+// declare ulong
+    val mUlong: ULong
+// declare float
+    val myFloat: Float
+// declare double
+    val myDouble: Double
+//declare boolean
+    val myBoolean: Boolean
+// declare char
+    val myChar: Char
+
+
+// Initialization of variables
+
+    myString = "Hello World"
+    myInt = 10
+    myLong = 1000000000000000
+    myByte = 1
+    myShort = 2
+    myUbyte = 3u
+    myUshort = 4u
+    myUint = 5u
+    mUlong = 6uL
+    myFloat = 1.0f
+    myDouble = 2.7
+    myBoolean = true
+    myChar = 'A'
+
+
+
+
+
+}
